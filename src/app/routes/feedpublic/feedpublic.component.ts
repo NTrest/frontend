af4cdc43edb68267a364d137968131288514404b
Ben Brecher <bb16n@my.fsu.edu>
2017-11-18 13:59:00 -0500

    Added form to Feedpublic

--- src/app/routes/feedpublic/feedpublic.component.ts
+++ src/app/routes/feedpublic/feedpublic.component.ts
@@ -2,19 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 import { FeedItem } from '../../classes/feedItem';
 
+import {NgForm} from '@angular/forms';
+
 @Component({
   selector: 'app-feedpublic',
   templateUrl: './feedpublic.component.html',
   styleUrls: ['./feedpublic.component.less']
 })
 export class FeedpublicComponent implements OnInit {
-
-  feed: FeedItem[];
+  model: any = {};
+  feed: FeedItem[] = [];
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sendmsg(): void {
+    this.feed.push({username: "USER", message: this.model.status});
   }
 
 }
