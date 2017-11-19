9be125d18deb9c8660a5d6c826a93f68429bafb2
Matt Lichtenstein <matt@Matts-MacBook-Pro.local>
2017-11-18 19:32:58 -0500

    Added models

--- src/app/routes/register/register.component.ts
+++ src/app/routes/register/register.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../classes/user';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.less']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  model: any = {};
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.register(this.model, (registered) => {
+      if (!registered) {
+        // SHOW ERROR!!!!
+        return;
+      }
+
+    });
   }
 
 }
